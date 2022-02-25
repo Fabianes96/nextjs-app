51894c4220f084e30b7269cf5940c13fe3b8c6a4
Fabian Higuita <fabian.higuitaa@perficient.com>
2022-02-25 14:51:24 -0500

    Add connection to db & fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,6 @@
 import MeetupList from "../components/meetups/MeetupList";
+import {MongoClient} from 'mongodb'
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "First meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/M%C3%BCnchen_Blick_vom_Neuen_Rathaus_zu_St_Peter_September_2017.jpg/1280px-M%C3%BCnchen_Blick_vom_Neuen_Rathaus_zu_St_Peter_September_2017.jpg",
-    address: 'Some address 5, 12345 Some city',
-    description: 'This is a first meetup'
-  },
-  {
-    id: "m2",
-    title: "Second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/M%C3%BCnchen_Blick_vom_Neuen_Rathaus_zu_St_Peter_September_2017.jpg/1280px-M%C3%BCnchen_Blick_vom_Neuen_Rathaus_zu_St_Peter_September_2017.jpg",
-    address: 'Some address 5, 12345 Some city',
-    description: 'This is a second meetup'
-  },
-];
 const HomePage = (props) => {  
   return( 
     <MeetupList meetups={props.meetups} />
@@ -33,9 +16,23 @@ const HomePage = (props) => {
 //   }
 // }
 export async function getStaticProps(){
+  //fetch data from an API
+  const client = await MongoClient.connect('mongodb+srv://fabianes96:x2bDzRrdaq7R8lSf@cluster0.9clfq.mongodb.net/meetups?retryWrites=true&w=majority');
+  const db = client.db();
+
+  const meetupsCollections = db.collection('meetups');
+
+  const meetups = await meetupsCollections.find().toArray()
+
+  client.close();
   return{
     props:{
-      meetups: DUMMY_MEETUPS
+      meetups: meetups.map(meetup => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      }))
     }
   }
 }
